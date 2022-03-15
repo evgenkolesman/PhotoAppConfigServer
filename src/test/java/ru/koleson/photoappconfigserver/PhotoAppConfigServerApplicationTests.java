dd233a442ea89fd833e2a8930745fb3d8bca298a
evgenkolesman <glavstroi_e@mail.ru>
2022-03-15 12:19:46 +0300

    Updating projects for messaging about services

---
+++ src/test/java/ru/koleson/photoappconfigserver/PhotoAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.koleson.photoappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
